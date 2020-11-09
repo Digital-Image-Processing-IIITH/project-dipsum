f029897c5440072a7ad708ba0f39f52fdaf6b1cd
siddhantb <siddhant2697@gmail.com>
2020-11-09 07:26:31 +0530

    Update the pipeline for testing all the images

--- src/opencv_pipeline_main.py
+++ src/opencv_pipeline_main.py
@@ -9,6 +9,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--descpt_id', default='sift', help='Name of the descriptor to use. Options are: [SIFT, SURF, ORB]')
 parser.add_argument('--descriptors', default='images/', help='Path to the pickle file containing descriptors for database images')
 parser.add_argument('--query', default='images/query/amstel_light.jpg', help='Path to the query image')
+parser.add_argument('--single_query', type=int, default=0, help='Flag for using single or all the queries. Use 0 for all queries and 1 for single query')
+parser.add_argument('--q_list', default='/Users/siddhantbansal/Desktop/IIIT-H/Courses/DIP/Project/project-dipsum/images/query', help='Path to the folder containing all the query images')
 args = parser.parse_args()
 print('[INFO] {}'.format(args))
 
@@ -16,6 +18,13 @@ args.descpt_id = args.descpt_id.lower()
 assert args.descpt_id in ['surf', 'sift', 'orb'], "Invalid descriptor id. Valid options are: [SIFT, SURF, ORB]"
 args.descriptors = os.path.join(args.descriptors, 'database_{}.pkl'.format(args.descpt_id))
 assert os.path.exists(args.descriptors), "Please generate the descriptors for database images."
+
+if args.single_query == 0:
+    query_list_ = os.listdir(args.q_list)
+    query_list = [os.path.join(args.q_list, item) for item in query_list_]
+else:
+    query_list_ = [args.query.split('/')[-1].split('.')[0]]
+    query_list = [args.query]
 
 with open(args.descriptors, 'rb') as file:
     data = pickle.load(file) 
@@ -29,17 +38,18 @@ elif args.descpt_id == 'surf':
 else:
     feature_extractor = cv2.ORB_create() # Limiting number of keypoints to 500
 
-image_gray = cv2.imread(args.query, 0)
-image_gray = cv2.resize(image_gray,(400,300))
-_, query_descriptor = feature_extractor.detectAndCompute(image_gray, None)
+for master_count, query_image in enumerate(query_list):
+    image_gray = cv2.imread(query_image, 0)
+    image_gray = cv2.resize(image_gray,(400,300))
+    _, query_descriptor = feature_extractor.detectAndCompute(image_gray, None)
 
-bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
-average_dis = []
-for count, match_descriptor in enumerate(tqdm(database_descriptors)):
-    matches = bf.match(match_descriptor,query_descriptor)
-    match_d =[x.distance for x in matches]
-    average_dis.append(np.mean(match_d))
-least_dist = min(average_dis)
-least_name = descriptor_names[average_dis.index(least_dist)]
-
-print('[INFO] Least distance is {}.\nName of the label with least distance is: {}.'.format(least_dist, least_name))
+    bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+    average_dis = []
+    for count, match_descriptor in enumerate(database_descriptors):
+        matches = bf.match(match_descriptor, query_descriptor)
+        match_d =[x.distance for x in matches]
+        average_dis.append(np.mean(match_d))
+    least_dist = min(average_dis)
+    least_name = descriptor_names[average_dis.index(least_dist)]
+    query_name = query_list_[master_count]
+    print('[INFO] {} --> {}; Distance: {}'.format(query_name, least_name, least_dist))
