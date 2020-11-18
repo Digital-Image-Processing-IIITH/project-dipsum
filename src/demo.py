27b60427f65a8722d36df912324376c02b75c64b
siddhantb <siddhant2697@gmail.com>
2020-11-18 10:49:01 +0530

    Add visualization capability to the demo.py and fix sift_visualization.py

--- src/demo.py
+++ src/demo.py
@@ -1,8 +1,10 @@
+import pdb
 import cv2
 import sift
 import argparse
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-l', default='lookup/main_sift.pkl', help='Path of the pre computed descriptor file')
@@ -13,6 +15,9 @@ GOOD_MATCH_THRESH = 10
 
 descriptor_file = args.l
 query_image = args.q
+matches_track = None
+database_image = None
+distances_track = list()
 
 with open(descriptor_file, 'rb') as file:
     database_descriptors = pickle.load(file)
@@ -34,10 +39,29 @@ for database_name in database_descriptors.keys():
     counts = list()
     for i, (m, n) in enumerate(matches):
         if m.distance < 0.7 * n.distance:
-            distances.append(m.distance)
+            distances.append(m)
     counts.append(len(distances))
     if len(distances) != 0:
         if np.max(counts) > max_count:
             max_count = np.max(counts)
             max_count_name = database_name.split('/')[-1].split('.')[0]
-print("[INFO] Label for {}'s query image  is: {}".format(query_image.split('/')[-1].split('.')[0], max_count_name))
+            matches_track = matches
+            database_image = database_name.split('/')[-1]
+            distances_track = distances
+print("[INFO] Label for {}'s query image is: {}".format(query_image.split('/')[-1].split('.')[0], max_count_name))
+
+print('\n[INFO] Preparing the visualization...\n')
+extension_list = ['.jpeg', '.png', '.jpg', '.JPEG', '.PNG', '.JPG']
+for extension in extension_list:
+    img_d = cv2.imread('images/database/{}{}'.format(database_image, extension), 0)    
+    if img_d is not None:
+        break
+
+keypoints_2, _ = sift.detectAndCompute(img_d, verbose=True)
+try:
+    matches = sorted(distances_track, key = lambda x:x.distance)
+    matched_image = cv2.drawMatches(img_q_gray, keypoints_1, img_d, keypoints_2, matches, None, flags=2)
+except:
+    pdb.set_trace()
+plt.imshow(matched_image)
+plt.show()
--- src/sift_visualization.py
+++ src/sift_visualization.py
@@ -34,8 +34,8 @@ img_d_gray = cv2.imread(database_images,0)
 img_q_gray = cv2.imread(query_images,0)
 
 #use sift to get keypoints and descriptors
-keypoints_1, descriptors_1 = sift.detectAndCompute(img_d_gray, verbose=True)
-keypoints_2, descriptors_2 = sift.detectAndCompute(img_q_gray, verbose=True)
+keypoints_1, descriptors_1 = sift.detectAndCompute(img_q_gray, verbose=True)
+keypoints_2, descriptors_2 = sift.detectAndCompute(img_d_gray, verbose=True)
 
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
@@ -49,9 +49,7 @@ for i, (m, n) in enumerate(matches):
         distances.append(m)
 
 matches = sorted(distances, key = lambda x:x.distance)
-matched_image = cv2.drawMatches(img_d, keypoints_1, img_q, keypoints_2, matches, img_q, flags=2)
+matched_image = cv2.drawMatches(img_q_gray, keypoints_1, img_d_gray, keypoints_2, matches, None, flags=2)
 plt.imshow(matched_image)
 plt.show()
 # cv2.imwrite("output.jpg",cv2.cvtColor(matched_image, cv2.COLOR_RGB2BGR))
-
-
